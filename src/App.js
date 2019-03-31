9d98e48fd55ed8c48ae106c1867ce35ec5344075
Chacko <chackop@gmail.com>
2019-03-31 13:22:34 +0100

    item detail and attempt at mobile slider container (+3 squashed commits) Squashed commits: [fd68679] applied responsiveness [4b6c4ce] replaced mockdata with api data [361c5ea] hooked up api

--- src/App.js
+++ src/App.js
@@ -3,31 +3,52 @@ import "./App.scss";
 import Carouseltem from "./components/Carousel/Carouseltem";
 import mockdata from "./data/mockdata";
 
+const API = "https://pixabay.com/api/";
+const APIKEY = "?key=9656065-a4094594c34f9ac14c7fc4c39";
+const APIQUERY = "&q=beautiful+landscape&image_type=photo";
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       properties: mockdata.properties,
-      property: mockdata.properties[0]
+      currProperty: mockdata.properties[0]
     };
   }
 
+  componentDidMount() {
+    fetch(API + APIKEY + APIQUERY)
+      .then(response => response.json())
+      .then(data => {
+        let filteredData = data.hits.slice(10, 20).map((fdata, i) => {
+          return {
+            index: i,
+            imageURL: fdata.webformatURL,
+            user: fdata.user,
+            likes: fdata.likes
+          };
+        });
+        this.setState({ properties: filteredData });
+      })
+      .catch(e => console.log("fetch rejection", e));
+  }
+
   nextItem = () => {
-    const newIndex = this.state.property.index + 1;
+    const newIndex = this.state.currProperty.index + 1;
     this.setState({
-      property: mockdata.properties[newIndex]
+      currProperty: this.state.properties[newIndex]
     });
   };
 
   prevItem = () => {
-    const newIndex = this.state.property.index - 1;
+    const newIndex = this.state.currProperty.index - 1;
     this.setState({
-      property: mockdata.properties[newIndex]
+      currProperty: this.state.properties[newIndex]
     });
   };
 
   render() {
-    const { properties, property } = this.state;
+    const { properties, currProperty } = this.state;
     return (
       <div className="App">
         <header>
@@ -36,31 +57,35 @@ class App extends Component {
           </section>
         </header>
 
-        <div className="page">
-          <div className={`carousel-slider active-slide-${property.index}`}>
-            <div
-              className="carousel-slide-wrapper"
-              style={{
-                transform: `translateX(-${property.index *
-                  (100 / properties.length)}%)`
-              }}
-            >
-              {properties.map((property) => (
-                <Carouseltem props={property} />
-              ))}
-            </div>
+        <div className={`carousel-slider active-slide-${currProperty.index}`}>
+          <div
+            className="carousel-slide-wrapper"
+            style={{
+              transform: `translateX(-${currProperty.index *
+                (100 / properties.length)}%)`
+            }}
+          >
+            {properties.map(property => (
+              <Carouseltem
+                key={`carousel-item-key-${property.index}`}
+                props={property}
+              />
+            ))}
           </div>
         </div>
-        <div className="carousel-button">
+
+        <div className="carousel-button-container">
           <button
             onClick={() => this.prevItem()}
-            disabled={property.index === 0}
+            disabled={currProperty.index === 0}
+            className="carousel-button"
           >
             Prev
           </button>
           <button
             onClick={() => this.nextItem()}
-            disabled={property.index === mockdata.properties.length - 1}
+            disabled={currProperty.index === properties.length - 1}
+            className="carousel-button"
           >
             Next
           </button>
--- src/components/Carousel/Carouseltem.js
+++ src/components/Carousel/Carouseltem.js
@@ -1,16 +1,17 @@
-import React from 'react'
+import React from "react";
 
-const CarouselItem = ({props}) => {
-    const {index, imageURL, alttext} = props;
-    return (
-        <div id={`carousel-item-${index}`} className="carousel-item">
-            <img src={imageURL} alt={alttext} />
-            <div className="carousel-item-detail">
-                <span className="detail-item">{index+1}</span>
-            </div>
-        </div>
-    )
-}
+const CarouselItem = ({ props }) => {
+  const { index, imageURL, user, likes } = props;
+  return (
+    <div id={`carousel-item-${index}`} className="carousel-item">
+      <img src={imageURL} alt="" />
+      <div className="carousel-item-detail">
+        <span className="detail-item">{`Index: ${index + 1}`}</span>
+        <span className="detail-item">{`User: ${user}`}</span>
+        <span className="detail-item">{`Likes: ${likes}`}</span>
+      </div>
+    </div>
+  );
+};
 
-export default CarouselItem
-
+export default CarouselItem;
